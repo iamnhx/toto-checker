ca05e52bb2992a92f9d0a10aa45ce96972821214
iamnhx <98194547+iamnhx@users.noreply.github.com>
2023-01-20 22:00:46 +0800

    Change list to using set.intersection
    Using sets instead of lists because the order of the numbers doesn't matter, and you only care about the numbers that match between the ticket numbers and the draw results.
    
    Use the set.intersection() method to find the common elements between two sets. This method returns a new set containing only items that exist in both the original set and the set passed as an argument.

--- toto_check.py
+++ toto_check.py
@@ -5,16 +5,17 @@ def toto_check():
     open(date + ' TOTO Results.txt', 'w').close()
     draw_results = set(input("Enter the draw results(comma separated): ").split(","))
     draw_results = {int(i) for i in draw_results}
-    with open("numbers.txt", "r") as f:
-        for line in f:
-            ticket_numbers = set(line.strip().split(","))
-            ticket_numbers = {int(i) for i in ticket_numbers}
-            match = draw_results.intersection(ticket_numbers)
-            counter = len(match)
-            with open(date + ' TOTO Results.txt', 'a+', encoding='utf-8') as f:
-                f.write("Draw results: " + str(draw_results) + "\n")
-                f.write("Ticket Number: " + str(ticket_numbers) + "\n")
-                f.write("Number of matches: " + str(counter) + "\n")
+    with open(date + ' TOTO Results.txt', 'a+', encoding='utf-8') as f_out:
+        f_out.write("Draw results: " + str(draw_results) + "\n")
+        with open("numbers.txt", "r") as f_in:
+            for line in f_in:
+                ticket_numbers = set(line.strip().split(","))
+                ticket_numbers = {int(i) for i in ticket_numbers}
+                match = draw_results.intersection(ticket_numbers)
+                counter = len(match)
+                f_out.write("Ticket Number: " + str(ticket_numbers) + "\n")
+                f_out.write("Number of matches: " + str(counter) + "\n")
                 match = match if counter > 0 else "none"
-                f.write("Matched numbers: " + str(match) + "\n\n")
+                f_out.write("Matched numbers: " + str(match) + "\n\n")
+
 toto_check()
